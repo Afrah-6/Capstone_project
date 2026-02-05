81a0a6923128dc9a163ba6fd2914476b47715d6c
labuser <labuser@172.31.36.119>
2026-02-05 15:21:27 +0000

    Commit

--- src/test/java/stepdefinitions/dropdown.java
+++ src/test/java/stepdefinitions/dropdown.java
@@ -1,5 +1,96 @@
 package stepdefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class dropdown {
+	
+
+@Given("User is on home page")
+public void user_is_on_home_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on Books")
+public void user_clicks_on_books() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on {int}-{int} under Books by Budget")
+public void user_clicks_on_under_books_by_budget(Integer int1, Integer int2) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@Then("user clicks on first book and adds to cart")
+public void user_clicks_on_first_book_and_adds_to_cart() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on Fiction")
+public void user_clicks_on_fiction() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on Fiction-BestSellers under Best-Sellers")
+public void user_clicks_on_fiction_best_sellers_under_best_sellers() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on Non-Fiction")
+public void user_clicks_on_non_fiction() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on Best Books Of The Year under Browse")
+public void user_clicks_on_best_books_of_the_year_under_browse() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on Teens& Ya")
+public void user_clicks_on_teens_ya() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on RelationShip Stories Under Top Subjects")
+public void user_clicks_on_relation_ship_stories_under_top_subjects() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on Kids")
+public void user_clicks_on_kids() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on Akbar and Birbal  under Popular Series")
+public void user_clicks_on_akbar_and_birbal_under_popular_series() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on Exams")
+public void user_clicks_on_exams() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+@When("User clicks on Banking\\(IBPS, SBI) under Government Exams")
+public void user_clicks_on_banking_ibps_sbi_under_government_exams() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+}
+
+
 
 }
