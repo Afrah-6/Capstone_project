fff68798b7ffaa8f0049c0421fc8b50724c2d397
labuser <labuser@172.31.36.119>
2026-02-06 00:17:52 +0000

    commit

--- src/test/java/Pages/dropdown_method.java
+++ src/test/java/Pages/dropdown_method.java
@@ -1,6 +1,58 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 public class dropdown_method {
-	//linkText("Books)
 
+	    WebDriver driver;
+	    Actions actions;
+
+	    public void BooksPage (WebDriver driver) {
+	        this.driver = driver;
+	        this.actions = new Actions(driver);
+	    }
+
+	    // Locators
+	    By booksMenu = By.xpath("//a[text()='Books']");
+	    By firstBook = By.xpath("(//div[@class='book-item'])[1]");
+	    By addToCartBtn = By.id("add-to-cart");
+
+	    // Generic methods
+	    public void hoverOnBooks() {
+	        WebElement books = driver.findElement(booksMenu);
+	        actions.moveToElement(books).perform();
+	    }
+
+	    public void clickBooksByBudget(int min, int max) {
+	        driver.findElement(
+	            By.xpath("//a[contains(text(),'" + min + "-" + max + "')]")
+	        ).click();
+	    }
+
+	    public void clickFirstBookAndAddToCart() {
+	        driver.findElement(firstBook).click();
+	        driver.findElement(addToCartBtn).click();
+	    }
+
+	    public void clickMenuByText(String text) {
+	        driver.findElement(By.xpath("//a[text()='" + text + "']")).click();
+	    }
+	    
+	    public void clickFirstBookAndAddToCart1() {
+	        driver.findElement(firstBook).click();
+	        driver.findElement(addToCartBtn).click();
+	    }
+
+	    public void clickMenuContainsText(String text) {
+	        driver.findElement(By.xpath("//a[contains(text(),'" + text + "')]")).click();
+	    }
+	    
+	    public void clickFirstBookAndAddToCart2() {
+	        driver.findElement(firstBook).click();
+	        driver.findElement(addToCartBtn).click();
+	    }
+	    
 }
+
--- src/test/java/stepdefinitions/dropdown.java
+++ src/test/java/stepdefinitions/dropdown.java
@@ -1,94 +1,102 @@
 package stepdefinitions;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class dropdown {
+	WebDriver driver = DriverFactory.getDriver();
+    Actions actions = new Actions(driver);
 	
 
 @Given("User is on home page")
 public void user_is_on_home_page() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	 driver.get("https://www.yourbooksite.com");
 }
 
 @When("User clicks on Books")
 public void user_clicks_on_books() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	   WebElement books = driver.findElement(By.xpath("//a[text()='Books']"));
+       actions.moveToElement(books).perform();
+  
 }
 
 @When("User clicks on {int}-{int} under Books by Budget")
 public void user_clicks_on_under_books_by_budget(Integer int1, Integer int2) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	 WebElement budget =
+             driver.findElement(By.xpath("//a[contains(text(),'" + int1 + "-" + int2 + "')]"));
+     budget.click();
+ 
+   
 }
 
 @Then("user clicks on first book and adds to cart")
 public void user_clicks_on_first_book_and_adds_to_cart() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	   driver.findElement(By.xpath("(//div[@class='book-item'])[1]")).click();
+       driver.findElement(By.id("add-to-cart")).click();
+   
 }
 
 @When("User clicks on Fiction")
 public void user_clicks_on_fiction() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	 WebElement fiction = driver.findElement(By.xpath("//a[text()='Fiction']"));
+     actions.moveToElement(fiction).click().perform();
 }
+
+
+
 
 @When("User clicks on Fiction-BestSellers under Best-Sellers")
 public void user_clicks_on_fiction_best_sellers_under_best_sellers() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	 
+	        driver.findElement(By.xpath("//a[text()='Fiction BestSellers']")).click();
 }
 
 @When("User clicks on Non-Fiction")
 public void user_clicks_on_non_fiction() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	   driver.findElement(By.xpath("//a[text()='Non-Fiction']")).click();
 }
 
 @When("User clicks on Best Books Of The Year under Browse")
 public void user_clicks_on_best_books_of_the_year_under_browse() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	 driver.findElement(By.xpath("//a[text()='Best Books Of The Year']")).click();   
 }
 
 @When("User clicks on Teens& Ya")
 public void user_clicks_on_teens_ya() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+    driver.findElement(By.xpath("//a[contains(text(),'Teens')]")).click();
 }
 
 @When("User clicks on RelationShip Stories Under Top Subjects")
 public void user_clicks_on_relation_ship_stories_under_top_subjects() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+  
+	 driver.findElement(By.xpath("//a[text()='Relationship Stories']")).click();
 }
 
 @When("User clicks on Kids")
 public void user_clicks_on_kids() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	driver.findElement(By.xpath("//a[text()='Kids']")).click();   
 }
 
 @When("User clicks on Akbar and Birbal  under Popular Series")
 public void user_clicks_on_akbar_and_birbal_under_popular_series() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	 driver.findElement(By.xpath("//a[text()='Akbar and Birbal']")).click();   
 }
 
 @When("User clicks on Exams")
 public void user_clicks_on_exams() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	driver.findElement(By.xpath("//a[text()='Exams']")).click();
 }
 
 @When("User clicks on Banking\\(IBPS, SBI) under Government Exams")
 public void user_clicks_on_banking_ibps_sbi_under_government_exams() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	driver.findElement(By.xpath("//a[contains(text(),'IBPS')]")).click();
+   
 }
 
 
